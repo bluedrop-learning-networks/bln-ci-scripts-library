80f4388258eccbd3cab6a30d6665791dd5f2052a
Denis Iskandarov <denisiskandarov@bluedrop.com>
2020-12-01 12:11:56 +0400

    Migration to GH actions

--- test/test.bash
+++ test/test.bash
@@ -3,5 +3,9 @@
 set -x
 
 ./test/setup.bash
-./test/shellcheck.bash
+
+# deprecated in favor of dedicated github actions Step
+# or add conditional logic so human operators can run that step on their workstations if they wish
+# ./test/shellcheck.bash
+
 ./test/bats.bash
